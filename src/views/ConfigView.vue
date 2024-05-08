e19d5eab312035085da7386941a925020ee7738d
Nicolas Arias <101823845+NicolasAriasUfro@users.noreply.github.com>
2024-05-08 19:19:12 -0400

    port

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -1,11 +1,34 @@
 <script>
+import { useSerialPortStore } from "@/stores/SerialPortStore";
 export default {
   name: "ConfigView",
+  methods: {
+    useSerialPortStore,
+    async selectReciverData() {
+      await useSerialPortStore().connectSerialPort();
+    },
+    async changePort() {
+      await useSerialPortStore().disconnectSerialPort();
+      await useSerialPortStore().connectSerialPort();
+    },
+    async disconnectPort() {
+      await useSerialPortStore().disconnectSerialPort();
+    },
+  },
 };
 </script>
 
 <template>
-  <div>ConfigView</div>
+  <h1>Configuración</h1>
+  <h2>Conexión a un puerto COM</h2>
+  <v-btn @click="selectReciverData" v-if="!useSerialPortStore().isPortOpen"
+    >Elegir Puerto</v-btn
+  >
+  <v-container v-if="useSerialPortStore().isPortOpen">
+    <v-btn @click="changePort">Cambiar</v-btn>
+    <v-btn @click="disconnectPort">Desconectar</v-btn>
+  </v-container>
+  <v-divider class="ma-4"></v-divider>
 </template>
 
 <style scoped></style>
