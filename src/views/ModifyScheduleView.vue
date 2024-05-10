acf95bf09497917d208c966a6ad3f3cedee856b1
Nicolas Arias <101823845+NicolasAriasUfro@users.noreply.github.com>
2024-05-10 10:51:01 -0400

    V1

--- src/views/ModifyScheduleView.vue
+++ src/views/ModifyScheduleView.vue
@@ -7,12 +7,60 @@ export default {
   data() {
     return {
       nameSchedule: "",
+      tab: null,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     };
+  },
+  computed: {
+    items() {
+      //get name of every schedule
+      const schedules = [];
+      useScheduleStore().schedules.forEach((schedule) => {
+        schedules.push({ id: schedule.id, name: schedule.name });
+      });
+      return schedules;
+    },
+    timersOfSelectedSchedule() {
+      return useScheduleStore().schedules[useScheduleStore().selectedSchedule]
+        .timers;
+    },
+    selectedSchedule: {
+      get() {
+        return useScheduleStore().selectedSchedule;
+      },
+      set(value) {
+        useScheduleStore().selectedSchedule = value;
+      },
+    },
   },
 };
 </script>
 
 <template>
+  <v-card>
+    <v-toolbar color="primary">
+      <v-tabs v-model="tab" align-tabs="title">
+        <v-tab
+          v-for="item in items"
+          :key="item.id"
+          :text="item.name"
+          :value="item"
+        ></v-tab>
+      </v-tabs>
+    </v-toolbar>
+
+    <v-tabs-window v-model="tab">
+      <v-tabs-window-item v-for="item in items" :key="item" :value="item">
+        <v-card flat>
+          tab:{{ tab }}.
+          <br />
+          selected Schedule:{{ selectedSchedule }}.
+          <v-card-text v-text="timersOfSelectedSchedule"></v-card-text>
+        </v-card>
+      </v-tabs-window-item>
+    </v-tabs-window>
+  </v-card>
+
   <v-form>
     <v-text-field
       label="Nombre Cronograma"
