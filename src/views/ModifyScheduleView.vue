e991e2df269940933b0f9fb413e6c198c817e9f4
Nicolas Arias <101823845+NicolasAriasUfro@users.noreply.github.com>
2024-05-28 18:31:15 -0400

    seudo refactor

--- src/views/ModifyScheduleView.vue
+++ src/views/ModifyScheduleView.vue
@@ -15,21 +15,16 @@ export default {
     return {
       nameSchedule: "",
       tab: null,
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     };
   },
   computed: {
-    items() {
+    schedulesList() {
       //get name of every schedule
       const schedules = [];
       useScheduleStore().schedules.forEach((schedule) => {
         schedules.push({ id: schedule.id, name: schedule.name });
       });
       return schedules;
-    },
-    timersOfSelectedSchedule() {
-      return useScheduleStore().schedules[useScheduleStore().selectedSchedule]
-        .timers;
     },
     selectedSchedule: {
       get() {
@@ -49,24 +44,25 @@ export default {
     <v-toolbar color="primary">
       <v-tabs v-model="selectedSchedule" align-tabs="title">
         <v-tab
-          v-for="item in items"
-          :key="item.id"
-          :text="item.name"
-          :value="item"
+          v-for="schedule in schedulesList"
+          :key="schedule.id"
+          :text="schedule.name"
+          :value="schedule"
         ></v-tab>
       </v-tabs>
     </v-toolbar>
 
     <v-tabs-window v-model="selectedSchedule">
-      <v-tabs-window-item v-for="item in items" :key="item" :value="item">
-        <v-card flat>
-          tab:{{}}.
-          <br />
-          selected Schedule:{{ selectedSchedule }}. hola
-        </v-card>
+      <v-tabs-window-item
+        v-for="schedule in schedulesList"
+        :key="schedule"
+        :value="schedule"
+      >
+        <v-card flat> name schedule: {{ schedule.name }} </v-card>
       </v-tabs-window-item>
     </v-tabs-window>
   </v-card>
+  <v-divider class="ma-4"></v-divider>
 
   <v-form>
     <v-text-field
