7d2ac2b5677a578be044306cd8967a1bdf801402
Cucharoth <cucharoth@gmail.com>
2024-05-30 14:49:49 -0400

    feat: cronograma audio

--- src/components/GeneralManager.vue
+++ src/components/GeneralManager.vue
@@ -1,21 +1,44 @@
 <script>
 import { useScheduleStore } from "@/stores/SheduleStore";
 import { useTheme } from "vuetify/lib/framework.mjs";
+import { useAudioStore } from "@/stores/AudioStore";
 
 export default {
   data: () => ({
+    audioStore: useAudioStore(),
     theme: useTheme(),
+    audio: useAudioStore().audio,
   }),
   methods: {
     useScheduleStore,
+    playAudio() {
+        this.audio.volume = 0.2;
+        this.audio.loop = true;
+        this.audio.play();
+        this.audioStore.playing = true;
+    },
+    pauseAudio() {
+      this.audio.pause();
+    },
+    muteAudio() {
+      this.audio.muted = true;
+      this.audioStore.muted = true;
+    },
+    unMuteAudio() {
+      this.audio.muted = false;
+      this.audioStore.muted = false;
+    },
     generalPause() {
       useScheduleStore().paused = true;
+      this.pauseAudio();
     },
     generalPlay() {
       useScheduleStore().paused = false;
+      this.playAudio();
     },
     generalReset() {
       this.generalPause();
+      this.audioStore.playing = false;
       useScheduleStore().resetSchedule();
     },
     test() {
@@ -36,6 +59,15 @@ export default {
     <button @click.prevent="generalReset">
       <font-awesome-icon class="icon" icon="fa-solid fa-stop" />
     </button>
+    <v-divider class="ms-3 divider" vertical ></v-divider>
+    <div class="ml-10">
+      <button v-if="!audioStore.muted" @click.prevent="muteAudio">
+        <font-awesome-icon class="icon" icon="fa-solid fa-volume-high"/>
+      </button>
+      <button v-else @click.prevent="unMuteAudio">
+        <font-awesome-icon class="icon" icon="fa-solid fa-volume-xmark" />
+      </button>
+    </div>
   </div>
 </template>
 
@@ -55,7 +87,9 @@ button {
   font-size: larger;
   margin: 0 10px;
 }
-
+.divider {
+  border-color: v-bind("theme.current.colors.primary") !important;
+}
 .icon {
   color: var(--primary) !important;
 }
