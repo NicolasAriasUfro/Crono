5aad80e4862d7144406402de863f670ceee67534
Nicolas Arias <101823845+NicolasAriasUfro@users.noreply.github.com>
2024-05-31 10:24:35 -0400

    rename:cronoproyect

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { useSessionStore } from '@/stores/SessionStore';
-import { useAudioStore } from '@/stores/AudioStore';
-import router from '@/router';
+import { useSessionStore } from "@/stores/SessionStore";
+import { useAudioStore } from "@/stores/AudioStore";
+import router from "@/router";
 import ThemesSelector from "@/components/ThemesSelector.vue";
 
 const sessionStore = useSessionStore();
@@ -9,31 +9,31 @@ const userName = sessionStore.userName;
 const audioStore = useAudioStore();
 
 const logOut = () => {
-    audioStore.audio.pause();
-    sessionStore.$reset();
-    audioStore.$reset();
-    router.push({ name: "auth" });
+  audioStore.audio.pause();
+  sessionStore.$reset();
+  audioStore.$reset();
+  router.push({ name: "auth" });
 };
-
 </script>
 
 <template>
-    <v-toolbar class="px-5" color="primary">
-        <template v-slot:prepend>
-            <div class="text-h5">CronoCoso</div>
-        </template>
-        <v-divider class="ms-3" color="background" inset vertical ></v-divider>
-        <v-toolbar-title class="d-flex text-left align-center"> {{ userName }} </v-toolbar-title>
+  <v-toolbar class="px-5" color="primary">
+    <template v-slot:prepend>
+      <div class="text-h5">CronoProyect</div>
+    </template>
+    <v-divider class="ms-3" color="background" inset vertical></v-divider>
+    <v-toolbar-title class="d-flex text-left align-center">
+      {{ userName }}
+    </v-toolbar-title>
 
-        <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-        <v-toolbar-items>
-            <ThemesSelector />
+    <v-toolbar-items>
+      <ThemesSelector />
 
-            <v-divider vertical></v-divider>
+      <v-divider vertical></v-divider>
 
-            <v-btn @click="logOut" variant="text">Logout</v-btn>
-        </v-toolbar-items>
-        
-    </v-toolbar>
+      <v-btn @click="logOut" variant="text">Logout</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
